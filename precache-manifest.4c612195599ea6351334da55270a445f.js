c64b3451f3feda82e876a2939a88770d45f49ab8
Serge FON'O MEMGUE <fonomemgue@gmail.com>
2020-06-04 01:57:52 +0100

    Updates

---
+++ precache-manifest.4c612195599ea6351334da55270a445f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9117bb66c57420ea8161a632f6cf806d",
+    "url": "/http//:serge-cpu.github.io/GagoumConsulting/index.html"
+  },
+  {
+    "revision": "eec0e789f82e4d9d458a",
+    "url": "/http//:serge-cpu.github.io/GagoumConsulting/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "51db36f55cc3139f39af",
+    "url": "/http//:serge-cpu.github.io/GagoumConsulting/static/css/main.ec2e5b64.chunk.css"
+  },
+  {
+    "revision": "eec0e789f82e4d9d458a",
+    "url": "/http//:serge-cpu.github.io/GagoumConsulting/static/js/2.383076a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//:serge-cpu.github.io/GagoumConsulting/static/js/2.383076a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51db36f55cc3139f39af",
+    "url": "/http//:serge-cpu.github.io/GagoumConsulting/static/js/main.8bf7e5cd.chunk.js"
+  },
+  {
+    "revision": "d017430cb2a1ba9b3f35",
+    "url": "/http//:serge-cpu.github.io/GagoumConsulting/static/js/runtime-main.a5565f5c.js"
+  }
+]);
